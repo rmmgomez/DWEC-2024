ec17add3cd0156d3ca4f955456915e00d0ea787b
Rosa Medina <rmmgomez@gmail.com>
2024-09-29 23:38:50 +0200

    ready

--- JS/3rd week/12.dynamic modules/index.js
+++ JS/3rd week/12.dynamic modules/index.js
@@ -1,3 +1,4 @@
 const lang = /^es\b/.test(navigator.language) ? 'es' : 'en';
+// resto del código se bloqueará hasta que se resuelva la instrucción await
 const m = await import(`./messages-${lang}.js`);
 console.log(m.hello);
